08050c086187960e26b861ce7b2ae4e719fe53d9
Farhad Farahi <farhad.farahi@gmail.com>
2018-08-03 21:50:14 +0430

    fix input size for various devices

--- src/App.js
+++ src/App.js
@@ -6,12 +6,6 @@ import MenuItem from 'material-ui/MenuItem';
 import Button from '@material-ui/core/Button';
 import CardMedia from '@material-ui/core/CardMedia';
 import Card from '@material-ui/core/Card';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import InputAdornment from '@material-ui/core/InputAdornment';
-
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import AccountCircle from '@material-ui/icons/AccountCircle';
@@ -38,33 +32,7 @@ class App extends Component {
 
   render() {
     //var hello = <h1>hello world</h1>;
-      var buttonStyle = {
-          width: "100%",
-          marginTop: "2em",
-          backgroundColor: "#007bff",
-      };
-      var inputStyle = {
-          width: "16em",
-      };
-      var appbarStyle = {
-          backgroundColor: "#007bff",
-      };
-      var logoCardStyle = {
-          height: "6em",
-          width: "6em",
-          alignContent: "center",
-          align: "center",
-          marginLeft:"auto",
-          marginRight: "auto",
-          marginBottom: "2em",
-
-      };
-      var logoMediaStyle = {
-          height: "6em",
-          width: "6em",
-          alignContent: "center",
-
-      };
+      
 
 
       return (
@@ -93,26 +61,30 @@ class App extends Component {
                       style={logoMediaStyle}
                   />
               </Card>
+
               <Grid container spacing={8} alignItems="flex-end" >
-                  <Grid item >
+                  <Grid item style={iconStyle}>
                       <AccountCircle />
                   </Grid>
                   <Grid item style={inputStyle}>
-                      <TextField fullWidth id="input-with-icon-grid" label="Username"   />
+                      <TextField fullWidth id="input-with-icon-grid" label="Username" />
                   </Grid>
               </Grid>
               <Grid container spacing={8} alignItems="flex-end">
-                  <Grid item>
+                  <Grid item style={iconStyle}>
                       <Lock />
                   </Grid>
                   <Grid item style={inputStyle}>
                       <TextField fullWidth type={"password"} id="input-with-icon-grid" label="Password" />
                   </Grid>
               </Grid>
+
               <Button variant="contained" color="primary" style={buttonStyle} >
                   Login
               </Button>
-
+              <div style={registerStyle}>
+              Don't have an account? Register!
+              </div>
           </div>
       </MuiThemeProvider>
 
@@ -120,6 +92,55 @@ class App extends Component {
   }
 }
 const styles = {
-    navBar: {'top': AppBar.height}
-}
+    navBar: {
+        'top': AppBar.height,
+        'width': "50%",
+        'max-width': "200px",
+
+    }
+};
+
+const buttonStyle = {
+    width: "100%",
+    marginTop: "2em",
+    marginBottom: "1em",
+    backgroundColor: "#007bff",
+};
+const inputStyle = {
+    width: "18em",
+    flex: 1, // to make it responsive with required width
+   // maxWidth: "14em",
+    marginBottom: "1em",
+
+};
+const iconStyle = {
+    marginBottom: "1em",
+};
+const appbarStyle = {
+    backgroundColor: "#007bff",
+};
+const logoCardStyle = {
+    height: "6em",
+    width: "6em",
+    alignContent: "center",
+    align: "center",
+    marginLeft:"auto",
+    marginRight: "auto",
+    marginBottom: "2em",
+
+};
+const logoMediaStyle = {
+    height: "6em",
+    width: "6em",
+    alignContent: "center",
+
+};
+const registerStyle = {
+    fontFamily: "Roboto, Helvetica, Arial, sans-serif",
+    marginTop: "1em",
+    align: "center",
+    fontSize: "14px",
+    textAlign: "center",
+};
+
 export default App;
